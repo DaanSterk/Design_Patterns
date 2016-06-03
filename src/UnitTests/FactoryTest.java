b8348bbf16cbc3fc0b30431beb2341ce5df0d2ca
Jeffrey Vervoort <jeffreyvervoort@gmail.com>
2016-06-03 16:24:43 +0200

    Factory Tests

--- src/UnitTests/FactoryTest.java
+++ src/UnitTests/FactoryTest.java
@@ -4,16 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import circuit_gates.GateAND;
+import circuit_gates.GateNeutral;
 import context_circuit.GateFactory;
 import context_circuit.gates.Gate;
 
 public class FactoryTest {
 
 	@Test
-	public void CreateGate() {
-		Gate g = GateFactory.create("GateAND", "NODE3");
-		g.incrementInputCount();
-		assertEquals("Creation of NODE3 was succesfull", "NODE3", g.getName());
+	public void CreateGateAND() {
+		Gate g = GateFactory.create("NODE3", "GateAND");
+		assertThat(g, instanceOf(GateAND.class));
+	}
+	
+	@Test
+	public void CreateGateNeutral() {
+		Gate g = GateFactory.create("NODE3", "GateNeutral");
+		assertThat(g, instanceOf(GateNeutral.class));
 	}
 
 }
